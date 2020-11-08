e2742de53db195069254b9e16b35fa9aa40a4f95
Chong Teng <t13340033021@qq.com>
2020-11-08 16:13:58 -0500

    start

--- huobi/constant/test.py
+++ huobi/constant/test.py
@@ -4,10 +4,10 @@ if(os.path.exists("huobi/privateconfig.py")):
     g_api_key = p_api_key
     g_secret_key = p_secret_key
 else:
-    g_api_key="feaf06c0-00de3aa3-fe8b99ff-1qdmpe4rty"
-    g_secret_key="f5e1dda1-d33078ec-7115892b-f57e8"
+    g_api_key="xxxx06c0-00de3aa3-fe8b99ff-1qdmpe4rty"
+    g_secret_key="xxxxdda1-d33078ec-7115892b-f57e8"
 
 g_account_id = 12345678
 
-g_sub_uid = 87654321
+g_sub_uid = xx654321
 
